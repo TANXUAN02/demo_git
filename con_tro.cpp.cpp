c8ba80246a6e99f31a69dd471e1fb3fdbb65c979
TANXUAN <20020508@vnu.edu.vn>
2021-03-30 14:17:28 +0700

    add con_tro.cpp.cpp

---
+++ con_tro.cpp.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int c = 10;
+	cout << c << endl;
+	cout << &c << endl;
+	cout << *&c << endl;
+	int *xp =  &c;
+	cout << *xp << endl; // neu dung cout << xp chỉ ra địa chị
+
+	*xp = 100; // thay đỏi giá trị của địa chỉ gốc
+	cout << c;
+	float* pt{ 0 }; // con trỏ null
+	float* pt2; // con tro rác
+	pt2 = 0; // thành con trỏ null
+}
